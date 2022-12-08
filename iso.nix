3946d22e6c00e605396977b8bfe7f323b26f3c1d
SamLukeYes <samlukeyes123@gmail.com>
2022-12-08 13:06:15 +0800

    Enable tzupdate

--- iso.nix
+++ iso.nix
@@ -21,13 +21,16 @@
     }
   )];
 
-  services.xserver = {
-    enable = true;
-    displayManager = {
-      lightdm.enable = true;
-    };
-    desktopManager.deepin = {
+  services = {
+    tzupdate.enable = true;
+    xserver = {
       enable = true;
+      displayManager = {
+        lightdm.enable = true;
+      };
+      desktopManager.deepin = {
+        enable = true;
+      };
     };
   };
 
