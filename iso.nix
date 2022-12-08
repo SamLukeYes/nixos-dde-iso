49f7f7f5ddad14de2aec6036e075ce092788a134
SamLukeYes <samlukeyes123@gmail.com>
2022-12-08 13:41:54 +0800

    Revert "Enable tzupdate"
    This reverts commit 3946d22e6c00e605396977b8bfe7f323b26f3c1d.

--- iso.nix
+++ iso.nix
@@ -21,16 +21,13 @@
     }
   )];
 
-  services = {
-    tzupdate.enable = true;
-    xserver = {
+  services.xserver = {
+    enable = true;
+    displayManager = {
+      lightdm.enable = true;
+    };
+    desktopManager.deepin = {
       enable = true;
-      displayManager = {
-        lightdm.enable = true;
-      };
-      desktopManager.deepin = {
-        enable = true;
-      };
     };
   };
 
