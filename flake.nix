08bd1f8b1e7c1b35179e5e1239c77ff17c504936
SamLukeYes <samlukeyes123@gmail.com>
2023-06-06 10:29:04 +0800

    Switch to nixos-23.05

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "NixOS live image with DDE";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
     calamares-deepin = {
       flake = false;
       url = "github:wineee/calamares-nixos-extensions/deepin";
